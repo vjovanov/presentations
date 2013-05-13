c2f61d1cac5cc39b1055b9283aef5002c3e55119
Vojin Jovanovic <vojin.jovanovic@epfl.ch>
2013-05-13 18:14:47 +0200

    Fix.

--- 2013geecon/examples/src/main/scala/Virtualization.scala
+++ 2013geecon/examples/src/main/scala/Virtualization.scala
@@ -1,16 +1,17 @@
 
+
 object Virtualization {
-  
-  def __ifThenElse[T](c: Boolean, t: => T, e: => T): T = 
+
+  def __ifThenElse[T](c: Boolean, t: => T, e: => T): Unit =
     println(s"if ($c) $t else $e")
 
   def __whileDo[T](c: Boolean, t: => Unit) = {
     println(s"while($c) {")
-    t()
+    t
     println(s"}")
   }
 
-  def println(t: String): Unit = "println(\"&t\")"
+  // def println(t: String): Unit = Predef.println(s"println($t)")
 
   while (true) {
   	if (true) println("true") else println("false")
