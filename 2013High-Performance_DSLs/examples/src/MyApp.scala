63e576897c650be6618b382a93c1d438549d7182
Vojin Jovanovic <gvojin@gmail.com>
2014-06-18 22:22:31 +0200

    Presentations finally in one place.

---
+++ 2013High-Performance_DSLs/examples/src/MyApp.scala
@@ -0,0 +1,33 @@
+//
+//object MyApp extends App {
+//  println("Zdravo svete!")
+//}
+//
+//object MyApp {
+//  def main(args: Array[String]) = {
+//    println("Zdravo svete!")
+//  }
+//}
+//
+//
+//object MyApp {
+//  def main(args: Array[String]) {
+//    val user: String = args(0)
+//    println(s"Hello $user")
+//  }
+//}
+//
+//class StringArrayFactory {
+//  def create: Array[String] = {
+//    val array: Array[String] =
+//      Array[String]("1", "2", "3")
+//    array
+//  }
+//}
+//
+//// Scala
+//class Person(
+//  var name: String,
+//  var age: Int
+//)
+//
